c45964cdded8f5831c1bf4cbf1621935eba2dcdf
xin zhao <xin.zhao@ebgames.com>
2018-04-06 01:01:47 +1000

    thread safe

--- Example.Cache.Tests/LRUCacheTests.cs
+++ Example.Cache.Tests/LRUCacheTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Example.Cache.Tests
 {
@@ -28,6 +31,88 @@ namespace Example.Cache.Tests
             Assert.IsFalse(result, "key 4 should be the least recently used one as key 3 has been updated.");
             LRUCache.TryGetValue(3, out value);
             Assert.AreEqual(value, 'x', "the value of key 1 should be 'x'");
+        }
+
+        //should pass
+        [Test]
+        public void SafeMultiThreadTest()
+        {
+            ICache<int, char> LRUCache = new LRUCache<int, char>(6);
+            MultiThreadTest(LRUCache);
+        }
+
+        //should fail
+        [Test]
+        public void UnsafeMultiThreadTest()
+        {
+            ICache<int, char> LRUCache = new LRUCacheThreadNotSafe<int, char>(6);
+            MultiThreadTest(LRUCache);
+        }
+
+        private void MultiThreadTest(ICache<int, char> LRUCache)
+        {
+            var thread1 = new Thread(() =>
+            {
+                LRUCache.AddOrUpdate(1, 'a');
+                LRUCache.AddOrUpdate(2, 'b');
+                LRUCache.AddOrUpdate(3, 'c');
+            });
+            var thread2 = new Thread(() =>
+            {
+                LRUCache.AddOrUpdate(11, 'A');
+                LRUCache.AddOrUpdate(12, 'B');
+                LRUCache.AddOrUpdate(13, 'C');
+            });
+
+            //Thread1 and Thread2 add data into Cache simultaneously.
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+
+            //After Thread1 and Thread 2 finish, all data can be retrieved.
+            char value;
+            bool result;
+            result = LRUCache.TryGetValue(1, out value);
+            Assert.IsTrue(result, "key 1 should be in Cache");
+            Assert.AreEqual(value, 'a', "the value of key 1 should be 'a'");
+            result = LRUCache.TryGetValue(2, out value);
+            Assert.IsTrue(result, "key 2 should be in Cache");
+            Assert.AreEqual(value, 'b', "the value of key 2 should be 'b'");
+            result = LRUCache.TryGetValue(3, out value);
+            Assert.IsTrue(result, "key 3 should be in Cache");
+            Assert.AreEqual(value, 'c', "the value of key 3 should be 'c'");
+            result = LRUCache.TryGetValue(11, out value);
+            Assert.IsTrue(result, "key 11 should be in Cache");
+            Assert.AreEqual(value, 'A', "the value of key 11 should be 'A'");
+            result = LRUCache.TryGetValue(12, out value);
+            Assert.IsTrue(result, "key 12 should be in Cache");
+            Assert.AreEqual(value, 'B', "the value of key 12 should be 'B'");
+            result = LRUCache.TryGetValue(13, out value);
+            Assert.IsTrue(result, "key 13 should be in Cache");
+            Assert.AreEqual(value, 'C', "the value of key 13 should be 'C'");
+
+            //Add new data to wash out old data 
+            LRUCache.AddOrUpdate(21, 'a');
+            LRUCache.AddOrUpdate(22, 'a');
+            LRUCache.AddOrUpdate(23, 'a');
+            LRUCache.AddOrUpdate(24, 'a');
+            LRUCache.AddOrUpdate(25, 'a');
+            LRUCache.AddOrUpdate(26, 'a');
+
+            //Old data should not be in cache
+            result = LRUCache.TryGetValue(1, out value);
+            Assert.IsFalse(result, "key 1 should NOT be in Cache");
+            result = LRUCache.TryGetValue(2, out value);
+            Assert.IsFalse(result, "key 2 should NOT be in Cache");
+            result = LRUCache.TryGetValue(3, out value);
+            Assert.IsFalse(result, "key 3 should NOT be in Cache");
+            result = LRUCache.TryGetValue(11, out value);
+            Assert.IsFalse(result, "key 11 should NOT be in Cache");
+            result = LRUCache.TryGetValue(12, out value);
+            Assert.IsFalse(result, "key 12 should NOT be in Cache");
+            result = LRUCache.TryGetValue(13, out value);
+            Assert.IsFalse(result, "key 13 should NOT be in Cache");
         }
     }
 }
--- Example.Cache/LRUCache.cs
+++ Example.Cache/LRUCache.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace Example.Cache
 {
+    /// <summary>
+    /// Least Recently Used (LRU) Cache management
+    /// When the cache is constructed, it should take as an argument the maximum number of elements stored in the cache.
+    /// When an item is added to the cache, a check should be run to see if the cache size exceeds the maximum number of elements permitted. 
+    /// If this is the case, then the least recently added/updated/retrieved item should be evicted from the cache.
+    /// All operations, including cache eviction, must have O(1) time complexity.
+    /// </summary>
     public class LRUCache<TKey, TValue> : ICache<TKey, TValue>
     {
         //node in double linked List 
-        internal class LRUCacheNode
+        protected internal class LRUCacheNode
         {
             public TKey Key { get; set; }
             public TValue Value { get; set; }
@@ -16,10 +24,11 @@ namespace Example.Cache
             public LRUCacheNode(){}
         };
 
-        private Dictionary<TKey, LRUCacheNode> _dictionary = new Dictionary<TKey, LRUCacheNode>();
-        private LRUCacheNode _head = new LRUCacheNode();
-        private LRUCacheNode _tail = new LRUCacheNode();
-        private readonly int _capacity = 0; //maximum number of elements permitted
+        Dictionary<TKey, LRUCacheNode> _dictionary = new Dictionary<TKey, LRUCacheNode>();
+        protected LRUCacheNode _head = new LRUCacheNode();
+        protected LRUCacheNode _tail = new LRUCacheNode();
+        protected readonly int _capacity = 0; //maximum number of elements permitted
+        private readonly Object _lockThis = new Object();
 
         public LRUCache(int capacity)
         {
@@ -28,28 +37,6 @@ namespace Example.Cache
             _head.Next = _tail;
             _tail.Prev = _head;
             _tail.Next = null;
-        }
-
-        public void AddOrUpdate(TKey key, TValue value)
-        {
-            if (_dictionary.ContainsKey(key) == false) //key is not in cache
-            {
-                if (_dictionary.Count == _capacity) // cache is full
-                    RemoveFromTail();
-
-                LRUCacheNode node = new LRUCacheNode();
-                node.Key = key;
-                node.Value = value;
-                _dictionary[key] = node;
-                InsertToFront(node);
-            }
-            else
-            {
-                LRUCacheNode node = _dictionary[key];
-                DetachNode(node);
-                node.Value = value;
-                InsertToFront(node);
-            }
         }
 
         public bool TryGetValue(TKey key, out TValue value)
@@ -69,23 +56,54 @@ namespace Example.Cache
             }
         }
 
-        private void RemoveFromTail()
+        virtual public void AddOrUpdate(TKey key, TValue value)
+        {
+            lock (_lockThis) 
+            {
+                AddOrUpdateThreadNotSafe(key, value);
+            }
+        }
+
+        protected void AddOrUpdateThreadNotSafe(TKey key, TValue value)
+        {
+            if (_dictionary.ContainsKey(key) == false) //key is not in cache
+            {
+                if (_dictionary.Count == _capacity) // cache is full
+                    RemoveFromTail();
+
+                LRUCacheNode node = new LRUCacheNode();
+                node.Key = key;
+                node.Value = value;
+                _dictionary.Add(key, node);
+                InsertToFront(node);
+            }
+            else
+            {
+                LRUCacheNode node = _dictionary[key];
+                DetachNode(node);
+                node.Value = value;
+                InsertToFront(node);
+            }
+        }
+
+        protected void RemoveFromTail()
         {
             LRUCacheNode node = _tail.Prev;
             DetachNode(node);
             _dictionary.Remove(node.Key);
         }
 
-        private void DetachNode(LRUCacheNode node)
+        protected void DetachNode(LRUCacheNode node)
         {
             node.Prev.Next = node.Next;
             node.Next.Prev = node.Prev;
         }
 
 
-        private void InsertToFront(LRUCacheNode node)
+        protected void InsertToFront(LRUCacheNode node)
         {
             node.Next = _head.Next;
+            Thread.Sleep(100); //keep this line to make a big change of error due to unsafe multiple threads. todo: delete this line in real applications.
             node.Prev = _head;
             _head.Next = node;
             node.Next.Prev = node;
